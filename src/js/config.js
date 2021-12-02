8c47f6b959d6f4e9dae3c2e2bdc6a7555deda3df
rahuljaglan <rahuljaglan035@gmail.com>
2021-12-02 21:31:48 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '64635a49-733d-4497-903d-85dcd6b3484f';
+export const MODAL_CLOSE_SEC = 2.5;
